cf114cf14c24328d21cd2c9ed37e75f536044d5e
FabrePlay <fabreplay24@gmail.com>
2025-06-21 21:43:22 -0400

    Revertir configuración de base de datos a PostgreSQL

--- zubacap-backend/config/middlewares.ts
+++ zubacap-backend/config/middlewares.ts
@@ -1,28 +1,8 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io'],
-          'media-src': ["'self'", 'data:', 'blob:'],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://localhost:5173']
-    }
-  },
+  'strapi::security',
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
